85323fb5b70f3a4fc81a235effbcee59ed06a844
BetmanSmall <betmansmall@mail.ru>
2024-04-10 10:46:26 +0300

    Practice4 add enters

--- MainClass.cs
+++ MainClass.cs
@@ -3,8 +3,10 @@ public static class MainClass {
     private static void Main(string[] args) {
         Practice4.Task01();
         Practice4.Task02();
+
         Console.ReadKey();
         Console.Clear();
+
         var gameOfLife = new Practice4_GameOfLife.GameOfLife();
     }
 }
--- Practice4.cs
+++ Practice4.cs
@@ -4,24 +4,32 @@
             Console.WriteLine("-Practice4--Task01-");
             Console.Write("Введите количество строк: ");
             int rowsCount = int.Parse(Console.ReadLine());
+
             Console.Write("Введите количество столбцов: ");
             int columnsCount = int.Parse(Console.ReadLine());
+
             Console.Write("Введите минимальное число для генерации: ");
             int randomMin = int.Parse(Console.ReadLine());
+
             Console.Write("Введите максимальное число для генерации: ");
             int randomMax = int.Parse(Console.ReadLine());
+
             Random random = new Random();
             int totalSum = 0;
             int[,] matrix = new int[rowsCount, columnsCount];
+
             for (int r = 0; r < rowsCount; r++) {
                 for (int c = 0; c < columnsCount; c++) {
+
                     int randomValue = matrix[r, c] = random.Next(randomMin, randomMax);
                     totalSum += randomValue;
+
                     Console.Write($"{randomValue} \t");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine($"totalSum: {totalSum}");
+
             return matrix;
         }
 
@@ -29,17 +37,22 @@
             Console.WriteLine("-Practice4--Task02-");
             int[,] matrix1 = Task01();
             int[,] matrix2 = Task01();
+
             if ((matrix1.Rank == matrix2.Rank) && matrix1.Rank == 2 &&
                 (matrix1.GetLength(0) == matrix2.GetLength(0) &&
                 (matrix1.GetLength(1) == matrix2.GetLength(1)))) {
+
                 int rowsCount = matrix1.GetLength(0);
                 int columnsCount = matrix1.GetLength(1);
                 int[,] matrix3 = new int[rowsCount, columnsCount];
+
                 for (int r = 0; r < rowsCount; r++) {
                     for (int c = 0; c < columnsCount; c++) {
+
                         int value1 = matrix1[r, c];
                         int value2 = matrix2[r, c];
                         int value3 = matrix3[r, c] = value1 + value2;
+
                         Console.Write($"{value3} \t");
                     }
                     Console.WriteLine();
