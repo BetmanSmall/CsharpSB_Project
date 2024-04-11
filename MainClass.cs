a6ab9dcca88989bfad84ba219afa96c2ab917176
BetmanSmall <betmansmall@mail.ru>
2024-04-11 06:01:42 +0300

    Practice5

--- MainClass.cs
+++ MainClass.cs
@@ -1,12 +1,7 @@
 ﻿namespace CsharpSB_Project;
 public static class MainClass {
     private static void Main(string[] args) {
-        Practice4.Task01();
-        Practice4.Task02();
-
-        Console.ReadKey();
-        Console.Clear();
-
-        var gameOfLife = new Practice4_GameOfLife.GameOfLife();
+        Practice5.Task01();
+        Practice5.Task02();
     }
 }
---
+++ Practice5.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace CsharpSB_Project; 
+public class Practice5 {
+    public static void Task01() {
+        Console.WriteLine("-Practice5--Task01-");
+        string text = Console.ReadLine();
+        string[] strings = SplitText(text, ' ');
+        PrintArray(strings);
+    }
+
+    private static string[] SplitText(string text, char separator) {
+        // string[] arrayStrings = text.Split(separator);
+        int whiteSpaceCount = 0;
+        foreach (char c in text) {
+            if (c.Equals(separator)) whiteSpaceCount++;
+        }
+        string[] arrayStrings = new string[whiteSpaceCount+1];
+        int wordIndex = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j < text.Length; j++) {
+            char c = text[j];
+            if (c != separator) {
+                stringBuilder.Append(c);
+            } else {
+                arrayStrings[wordIndex++] = stringBuilder.ToString();
+                stringBuilder.Clear();
+            }
+        }
+        if (stringBuilder.Length > 0) {
+            arrayStrings[wordIndex] = stringBuilder.ToString();
+        }
+        return arrayStrings;
+    }
+
+    private static void PrintArray(string[] arrayStrings) {
+        foreach (string arrayString in arrayStrings) {
+            Console.WriteLine(arrayString);
+        }
+    }
+
+    public static void Task02() {
+        Console.WriteLine("-Practice5--Task02-");
+        string text = Console.ReadLine();
+        string[] strings = ReversWords(text);
+        PrintArray(strings);
+    }
+
+    private static string[] ReversWords(string inputPhrase) {
+        string[] strings = SplitText(inputPhrase, ' ');
+        string[] reversStrings = new string[strings.Length];
+        for (int i = 0; i < strings.Length; i++) {
+            reversStrings[i] = strings[strings.Length - 1 - i];
+        }
+        return reversStrings;
+    }
+}
